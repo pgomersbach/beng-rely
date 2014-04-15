6d5f28830bf350f7851aa7b31990de8e2dd59812
Paul Gomersbach <p.gomersbach@rely.nl>
2014-04-15 11:32:13 +0000

    mv to site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,31 @@
+# Default configuration
+
+group { 'wheel': ensure => present, }
+
+augeas { "sudowheel":
+  context => "/files/etc/sudoers",
+  changes => [
+    "set spec[user = '%wheel']/user %wheel",
+    "set spec[user = '%wheel']/host_group/host ALL",
+    "set spec[user = '%wheel']/host_group/command ALL",
+    "set spec[user = '%wheel']/host_group/command/runas_user ALL",
+    "set spec[user = '%wheel']/host_group/command/tag NOPASSWD",
+    ],
+}
+
+class { '::ntp':
+  servers => [ '172.18.99.210', '172.18.99.211' ],
+}
+
+user { 'appbeheer':
+  ensure => present,
+}
+
+user { 'deploy':
+  ensure => present,
+}
+
+user { 'systeembeheer':
+  ensure => present,
+}
+
