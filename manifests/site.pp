215ee9fbd1223041bef05e225e929b63280f20bf
root <root@sjefkeschelm.beeldengeluid.nl>
2014-04-18 13:27:59 +0200

    fix syntax for lint

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,14 +2,14 @@
 
 group { 'wheel': ensure => present, }
 
-augeas { "sudowheel":
-  context => "/files/etc/sudoers",
+augeas { 'sudowheel':
+  context => '/files/etc/sudoers',
   changes => [
-    "set spec[user = '%wheel']/user %wheel",
-    "set spec[user = '%wheel']/host_group/host ALL",
-    "set spec[user = '%wheel']/host_group/command ALL",
-    "set spec[user = '%wheel']/host_group/command/runas_user ALL",
-    "set spec[user = '%wheel']/host_group/command/tag NOPASSWD",
+    'set spec[user = '%wheel']/user %wheel',
+    'set spec[user = '%wheel']/host_group/host ALL',
+    'set spec[user = '%wheel']/host_group/command ALL',
+    'set spec[user = '%wheel']/host_group/command/runas_user ALL',
+    'set spec[user = '%wheel']/host_group/command/tag NOPASSWD',
     ],
 }
 
@@ -31,13 +31,15 @@ class { 'vsftpd':
 
 file { '/etc/vsftpd/chroot_list':
   content => 'root',
+  require => Class['vsftpd'],
 }
 
 user { 'appbeheer':
-  ensure   => present,
+  ensure     => present,
   password   => '$6$whFu5R20$VZYxY42iExf8nd8yDIwXz6.K9D68BsreDcBUi9mqjO02x.m6i1HuD/uuHViqHvbWh.19.jDoMcMKOo1rtNaja.',
   groups     => 'wheel',
   managehome => true,
+  require    => Group['wheel'],
 }
 
 user { 'deploy':
@@ -45,12 +47,14 @@ user { 'deploy':
   password   => '$6$whFu5R20$VZYxY42iExf8nd8yDIwXz6.K9D68BsreDcBUi9mqjO02x.m6i1HuD/uuHViqHvbWh.19.jDoMcMKOo1rtNaja.',
   groups     => 'wheel',
   managehome => true,
+  require    => Group['wheel'],
 }
 
 user { 'systeembeheer':
-  ensure   => present,
+  ensure     => present,
   password   => '$6$whFu5R20$VZYxY42iExf8nd8yDIwXz6.K9D68BsreDcBUi9mqjO02x.m6i1HuD/uuHViqHvbWh.19.jDoMcMKOo1rtNaja.',
   groups     => 'wheel',
   managehome => true,
+  require    => Group['wheel'],
 }
 
