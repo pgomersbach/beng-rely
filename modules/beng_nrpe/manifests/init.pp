dd1e61ef91516968d4c8ad320fd923249a35bb03
root <root@sjefkeschelm.beeldengeluid.nl>
2014-04-22 11:10:20 +0200

    add more packages

--- modules/beng_nrpe/manifests/init.pp
+++ modules/beng_nrpe/manifests/init.pp
@@ -2,7 +2,7 @@
 # perl-Crypt-DES-2.05-9.el6.x86_64.rpm  vdl-nagios-plugins-1.4.15-2.x86_64.redhat.rpm
 # perl-Net-SNMP-5.2.0-4.el6.noarch.rpm  vdl-nrpe-2.12-4.x86_64.redhat.rpm
 # vdl-nagios-common-3.2-3.noarch.rpm    vdl-nrpe-plugin-2.12-4.x86_64.redhat.rpm
-# s404.ka.beeldengeluid.nl/nagios/depot/lin/nrpe-complied-rhel6/perl-Crypt-DES-2.05-9.el6.x86_64.rpm
+
 class beng_nrpe (
   $baseurl='http://s404.ka.beeldengeluid.nl/nagios/depot/lin'
 ){
@@ -31,6 +31,23 @@ class beng_nrpe (
     ensure   => installed,
     source   => "$rpmurl/vdl-nagios-common-3.2-3.noarch.rpm",
   }
+  
+  package { 'vdl-nagios-plugins':
+    provider => rpm,
+    ensure   => installed,
+    source   => "$rpmurl/vdl-nagios-plugins-1.4.15-2.x86_64.redhat.rpm",
+  }
 
+  package { 'vdl-nrpe':
+    provider => rpm,
+    ensure   => installed,
+    source   => "$rpmurl/vdl-nrpe-2.12-4.x86_64.redhat.rpm",
+  }
+
+  package { 'vdl-nrpe-plugin':
+    provider => rpm,
+    ensure   => installed,
+    source   => "$rpmurl/vdl-nrpe-plugin-2.12-4.x86_64.redhat.rpm",
+  }
 
 }
