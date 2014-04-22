138b05a6409b9bf1147e4763f8759a207bb51e6a
root <root@sjefkeschelm.beeldengeluid.nl>
2014-04-22 11:14:06 +0200

    add depend

--- modules/beng_nrpe/manifests/init.pp
+++ modules/beng_nrpe/manifests/init.pp
@@ -18,31 +18,31 @@ class beng_nrpe (
     provider => rpm,
     ensure   => installed,
     source   => "$rpmurl/perl-Net-SNMP-5.2.0-4.el6.noarch.rpm",
-  }
+  }->
 
   package { 'perl-Crypt-DES':
     provider => rpm,
     ensure   => installed,
     source   => "$rpmurl/perl-Crypt-DES-2.05-9.el6.x86_64.rpm",
-  }
+  }->
 
   package { 'vdl-nagios-common':
     provider => rpm,
     ensure   => installed,
     source   => "$rpmurl/vdl-nagios-common-3.2-3.noarch.rpm",
-  }
+  }->
   
   package { 'vdl-nagios-plugins':
     provider => rpm,
     ensure   => installed,
     source   => "$rpmurl/vdl-nagios-plugins-1.4.15-2.x86_64.redhat.rpm",
-  }
+  }->
 
   package { 'vdl-nrpe':
     provider => rpm,
     ensure   => installed,
     source   => "$rpmurl/vdl-nrpe-2.12-4.x86_64.redhat.rpm",
-  }
+  }->
 
   package { 'vdl-nrpe-plugin':
     provider => rpm,
