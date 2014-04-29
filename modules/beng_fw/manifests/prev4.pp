29f54356a2b664c17b1b6a279dcf1e9de06ed4e6
root <root@sjefkeschelm.beeldengeluid.nl>
2014-04-29 13:05:54 +0200

    typo

--- modules/beng_fw/manifests/prev4.pp
+++ modules/beng_fw/manifests/prev4.pp
@@ -25,7 +25,7 @@ class beng_fw::prev4 {
     provider => 'iptables',
   }
   firewall { '010 allow internal networks TCP':
-  #  dport    => $tcp_ports,
+    dport    => $tcp_ports,
 #    dport => [ '20','21','22','53','80','123','443','445','1556','5666','9100','9200','13720','13724'],
     proto    => 'tcp',
 #    source   => ${internal_nets},
@@ -44,7 +44,7 @@ class beng_fw::prev4 {
   }
   firewall { '020 allow internal networks UDP':
     dport    => $udp_ports,
-    dport => [ '53','123','161'],
+#    dport => [ '53','123','161'],
     proto    => 'udp',
 #    source   => ${internal_nets},
     source => [ '172.18.0.0/16','172.19.0.0/16' ],
