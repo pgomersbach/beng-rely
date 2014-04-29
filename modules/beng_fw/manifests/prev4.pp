d2901cececd8e95a4cad8d67e9cb4f71dd602b0a
root <root@sjefkeschelm.beeldengeluid.nl>
2014-04-29 13:10:32 +0200

    change scope

--- modules/beng_fw/manifests/prev4.pp
+++ modules/beng_fw/manifests/prev4.pp
@@ -1,8 +1,9 @@
-class beng_fw::prev4 {
-  $internal_nets = [ '172.18.0.0/16','172.19.0.0/16' ]
-  $tcp_ports = [ '20','21','22','53','80','123','443','445','1556','5666','9100','9200','13720','13724']
-  $tcp_range = '9300-9400'
-  $udp_ports = [ '53','123','161']
+class beng_fw::prev4( 
+  $internal_nets = [ '172.18.0.0/16','172.19.0.0/16' ],
+  $tcp_ports = [ '20','21','22','53','80','123','443','445','1556','5666','9100','9200','13720','13724'],
+  $tcp_range = '9300-9400',
+  $udp_ports = [ '53','123','161'],
+){
   Firewall {
     require => undef,
   }
@@ -26,28 +27,23 @@ class beng_fw::prev4 {
   }
   firewall { '010 allow internal networks TCP':
     dport    => $tcp_ports,
-#    dport => [ '20','21','22','53','80','123','443','445','1556','5666','9100','9200','13720','13724'],
     proto    => 'tcp',
-#    source   => ${internal_nets},
-    source => [ '172.18.0.0/16','172.19.0.0/16' ],
+    source   => $internal_nets,
     action   => 'accept',
     provider => 'iptables',
   }
   firewall { '015 allow internal networks TCP range':
 #    dst_range => ${tcp_range},
-    dport => '9300-9400',
+    dport    => $tcp_range,
     proto    => 'tcp',
-#    source   => ${internal_nets},
-    source => [ '172.18.0.0/16','172.19.0.0/16' ],
+    source   => $internal_nets,
     action   => 'accept',
     provider => 'iptables',
   }
   firewall { '020 allow internal networks UDP':
     dport    => $udp_ports,
-#    dport => [ '53','123','161'],
     proto    => 'udp',
-#    source   => ${internal_nets},
-    source => [ '172.18.0.0/16','172.19.0.0/16' ],
+    source   => $internal_nets,
     action   => 'accept',
     provider => 'iptables',
   }
