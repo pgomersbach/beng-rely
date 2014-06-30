599c34b95d34ca9b73f2a7d00e302143d193d655
bram <bram@bnux.nl>
2014-06-30 15:04:08 +0200

    1406 3254 Verzoek beperken toegang elasticsearch port 93xx op labs-beta

--- modules/beng_fw/manifests/prev4.pp
+++ modules/beng_fw/manifests/prev4.pp
@@ -4,7 +4,10 @@ class beng_fw::prev4 (
   $internal_netC = '178.249.248.128/25',
   $tcp_ports = [ '20','21','22','53','80','123','443','445','1556','5666','8000','9100','9200','13720','13724'],
   $tcp_public_ports = $beng_fw::tcp_public_ports,
-  $tcp_rangeA = '9300-9400',
+
+  $tcp_rangeA_ports = '9300-9400',
+  $tcp_rangeA_source = '172.19.53.11-172.19.53.15', # lbes1,lbes2,lbes3,lbas1,lbas2 (Rely#1406 3254)
+
   $tcp_rangeB = '8080-8082',
 
   $udp_ports = [ '53','123','161'],
@@ -12,6 +15,7 @@ class beng_fw::prev4 (
   Firewall {
     require => undef,
   }
+
   # Default firewall rules
   firewall { '000 accept all icmp':
     proto    => 'icmp',
@@ -30,6 +34,7 @@ class beng_fw::prev4 (
     action   => 'accept',
     provider => 'iptables',
   }
+
   # Check if public ports should be allowed
   if $tcp_public_ports != false {
       firewall { '003 accept all TCP':
@@ -39,6 +44,7 @@ class beng_fw::prev4 (
       provider => 'iptables',
     }
   }
+
   firewall { '010 allow internal netA TCP':
     dport    => $tcp_ports,
     proto    => 'tcp',
@@ -52,7 +58,7 @@ class beng_fw::prev4 (
     source   => $internal_netB,
     action   => 'accept',
     provider => 'iptables',
-    }
+  }
   firewall { '012 allow internal netC TCP':
     dport    => $tcp_ports,
     proto    => 'tcp',
@@ -60,27 +66,15 @@ class beng_fw::prev4 (
     action   => 'accept',
     provider => 'iptables',
   }
+
   firewall { '015 allow internal netA TCP range':
-    dport    => $tcp_rangeA,
+    dport    => $tcp_rangeA_ports,
     proto    => 'tcp',
-    source   => $internal_netA,
+    source   => $tcp_rangeA_source,
     action   => 'accept',
     provider => 'iptables',
   }
-  firewall { '016 allow internal netB TCP range':
-    dport    => $tcp_rangeA,
-    proto    => 'tcp',
-    source   => $internal_netB,
-    action   => 'accept',
-    provider => 'iptables',
-    }
-  firewall { '017 allow internal netC TCP range':
-    dport    => $tcp_rangeA,
-    proto    => 'tcp',
-    source   => $internal_netC,
-    action   => 'accept',
-    provider => 'iptables',
-    }
+
   firewall { '025 allow internal netA TCP rangeB':
     dport    => $tcp_rangeB,
     proto    => 'tcp',
@@ -94,14 +88,15 @@ class beng_fw::prev4 (
     source   => $internal_netB,
     action   => 'accept',
     provider => 'iptables',
-    }
+  }
   firewall { '027 allow internal netC TCP rangeB':
     dport    => $tcp_rangeB,
     proto    => 'tcp',
     source   => $internal_netC,
     action   => 'accept',
     provider => 'iptables',
-    }
+  }
+
   # UDP rules
   firewall { '020 allow internal netA UDP':
     dport    => $udp_ports,
