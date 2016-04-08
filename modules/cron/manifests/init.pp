2634171b51a07fb376186840be24c6d6cb3f71d5
marcosjerps <msjerps@beeldengeluid.nl>
2016-04-08 10:08:44 +0200

    added puppet module for cronjob handeling and setting up https://forge.puppet.com/rmueller/cron

---
+++ modules/cron/manifests/init.pp
@@ -0,0 +1,69 @@
+# Class: cron
+#
+# This class wraps *cron::install* for ease of use
+#
+# Parameters:
+#   manage_package - Can be set to disable package installation.
+#     Set to true to manage it, false to not manage it.
+#     Default: true
+#
+#   package_ensure - Can be set to a package version, 'latest', 'installed' or
+#     'present'.
+#     Default: installed
+#
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#   include 'cron'
+# or:
+#   class { 'cron':
+#     manage_package => false,
+# }
+#
+class cron (
+  $manage_package = true,
+  $package_ensure = 'installed',
+) {
+
+  if $manage_package {
+    class { '::cron::install':
+      package_ensure => $package_ensure,
+    }
+  }
+
+  # Create jobs from hiera
+  $cron_job=hiera_hash('cron::job', undef)
+  if $cron_job {
+    create_resources('cron::job',$cron_job)
+  }
+  
+  $cron_job_multiple=hiera_hash('cron::job::multiple', undef)
+  if $cron_job_multiple {
+    create_resources('cron::job::multiple',$cron_job_multiple)
+  }
+
+  $cron_job_hourly=hiera_hash('cron::hourly', undef)
+  if $cron_job_hourly {
+    create_resources('cron::hourly',$cron_job_hourly)
+  }
+
+  $cron_job_daily=hiera_hash('cron::daily', undef)
+  if $cron_job_daily {
+    create_resources('cron::daily',$cron_job_daily)
+  }
+
+  $cron_job_weekly=hiera_hash('cron::weekly', undef)
+  if $cron_job_weekly {
+    create_resources('cron::weekly',$cron_job_weekly)
+  }
+
+  $cron_job_monthly=hiera_hash('cron::monthly', undef)
+  if $cron_job_monthly {
+    create_resources('cron::monthly',$cron_job_monthly)
+  }
+
+}
+
---
+++ modules/cron/manifests/install.pp
@@ -0,0 +1,49 @@
+# Class: cron::install
+#
+# This class ensures that the distro-appropriate cron package is installed
+#
+# Parameters:
+#   package_ensure - Can be set to a package version, 'latest', 'installed' or
+#   'present'.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#   This class should not be used directly under normal circumstances
+#   Instead, use the *cron* class.
+#
+class cron::install (
+  $package_ensure = 'installed',
+) {
+
+  case $::operatingsystem {
+    /^(RedHat|CentOS|Amazon|OracleLinux)/: {
+      if versioncmp($::operatingsystemmajrelease, '5') <= 0 {
+        $package_name = 'vixie-cron'
+      } else {
+        $package_name = 'cronie'
+      }
+    }
+    'Gentoo': {
+      $package_name = 'sys-process/vixie-cron'
+    }
+    'Ubuntu': {
+      $package_name = 'cron'
+    }
+    'Debian': {
+      $package_name = 'cron'
+    }
+    default: {
+      $package_name = 'cron'
+    }
+  }
+
+  package { 'cron':
+    ensure => $package_ensure,
+    name   => $package_name,
+  }
+
+}
+
