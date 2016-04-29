0ee11df7b7ccea000802ac6e139318e749cd2c75
marcosjerps <msjerps@beeldengeluid.nl>
2016-04-29 14:33:16 +0200

    mv puppte directory naar www dir

--- files/cron/github_clone.sh
+++ files/cron/github_clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # cronjob script voor sync op sjefkeschelm ks files
-# 28-04-2016 mhj initail setup
+# 28-04-2016 mhj initial setup
+# 29-04-2016 mhj mv puppet directory naar www.directory
 puppet_source=https://github.com/relybv/beng-rely.git
 role=site
 if [ -d "/etc/puppet.orig" ]; then
@@ -20,4 +21,12 @@ while [ $? -ne 0 ]; do
   fi
   env GIT_SSL_NO_VERIFY=true git clone $puppet_source /etc/puppet
 done
+
+if [ -d "/var/www/html/beng-rely/" ]; then
+rm -rf /var/www/html/beng-rely/
+fi
+
+if [ -d "/etc/puppet" ]; then
+  mv  /etc/puppet /var/www/html/beng-rely/ 
+fi
 
