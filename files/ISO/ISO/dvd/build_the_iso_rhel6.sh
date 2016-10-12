fac14246466e316c81da53a97946670ff6ce1a8e
marcosjerps <msjerps@beeldengeluid.nl>
2016-10-12 09:38:08 +0200

    changed structure removed Double ISO/ from path

--- files/ISO/ISO/dvd/build_the_iso_rhel6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ~root/beng-rely/files/isolinux.cfg /ISO/dvd/isolinux/isolinux.cfg
-cd /ISO/dvd && mkisofs -o kickstart_lab.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -v -T isolinux/
